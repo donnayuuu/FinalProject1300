d400c6da1d2eddda93c5482a3ba0c497e4421e65
erinc525 <33532670+erinc525@users.noreply.github.com>
2017-11-27 22:11:43 -0500

    Add files via upload

---
+++ includes/header-navigation.php
@@ -0,0 +1,15 @@
+<div id="header">
+  <div id="logo">
+      <img alt="Logo" src="images/logo.jpg"/>
+  </div>
+
+  <div id="header-title"> That's How I Roll</div>
+  <div id=navigation-div>
+  <ul class="navigation">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="menu.php">Menu</a></li>
+    <li><a href="location.php">Locations</a></li>
+    <li><a href="contact.php">Contact</a></li>
+  </ul>
+  </div>
+</div>
